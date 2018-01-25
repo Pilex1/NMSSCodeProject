4ceae5bd66703302707812bc8e583ddf2fa1970f
Joanne Ng <34573903+mispelledname@users.noreply.github.com>
2018-01-26 07:24:52 +0800

    Update 9_Joanne_N.py

--- Project Euler/9_Joanne_N.py
+++ Project Euler/9_Joanne_N.py
@@ -5,14 +5,16 @@ For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 """
+## Note this is super inefficient and quite iffy :/
+
 
 a = 1
 b = 1
 c = 1
 
-for a in range (1,1000):
-    for b in range (1,1000):
-        for c in range (1,1000):
+for a in range (1,499):
+    for b in range (1,500):
+        for c in range (1,999):
             if a + b + c == 1000:
                 if a**2 + b**2 == c**2:
                     print(a*b*c)
