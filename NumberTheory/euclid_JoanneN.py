d91b4997b402e32b2d9240341a8549d7a35eee77
wen1now <wen00@live.com.au>
2018-01-27 10:47:55 +1000

    Update euclid_JoanneN.py
    Code is much cleaner this way

--- NumberTheory/euclid_JoanneN.py
+++ NumberTheory/euclid_JoanneN.py
@@ -1,14 +1,10 @@
 # Euclidean Algorithm to find gcd
 
 def gcd(a,b):
-    r = a % b
-    while r != 0:
-        a = b
-        b = r
-        r = a % b
+    while a % b != 0: a, b = b, a % b
     return b
 
 a = int(raw_input("What is the larger number? "))
 b = int(raw_input("What is the smaller number? "))
-gcd = gcd(a,b)
-print(gcd)
+#gcd = gcd(a,b) woah, woah, woah. This is really stupid - it overwrites the function
+print(gcd(a,b))
