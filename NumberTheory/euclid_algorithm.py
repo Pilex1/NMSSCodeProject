2f6a792161cd29bb501dbc587614f09b50b98bcf
mrenow <other.ezhui@gmail.com>
2018-01-29 14:31:40 +1100

    File doesnt run when imported
    Surround file specific functionality with if __name__ == "__main__":
    Not particularly important, but a good thing to keep in mind for more important files

--- NumberTheory/euclid_algorithm.py
+++ NumberTheory/euclid_algorithm.py
@@ -6,7 +6,9 @@ def gcd(a,b):
     while a % b != 0: a, b = b, a % b
     return b
 
-a = int(raw_input("What is the larger number? "))
-b = int(raw_input("What is the smaller number? "))
-#gcd = gcd(a,b) woah, woah, woah. This is really stupid - it overwrites the function
-print(gcd(a,b))
+# Only runs if the file is run directly
+if __name__ == "__main__": 
+    a = int(raw_input("What is the larger number? "))
+    b = int(raw_input("What is the smaller number? "))
+    #gcd = gcd(a,b) woah, woah, woah. This is really stupid - it overwrites the function
+    print(gcd(a,b))
