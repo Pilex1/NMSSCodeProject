3a11ced3e4b50ac26252c03899c4a2fb8e13a55c
Sonic the Hedge <ezhui@hotmail.com>
2018-02-05 22:33:12 +1100

    made ensuring the float precision faster, added sqrt and ln functions
    e^x and ln(x) seem to screw up for non integers which makes no sense in the context of floats
    Ill probably take a break because Ive been working on this for a few days but ill make sure I comment and explain everything first

--- NumberTheory/Numbers.py
+++ NumberTheory/Numbers.py
@@ -1,5 +1,6 @@
 __author__ = 'My Computer'
 import math
+import sys
 class APFloat:
     "Stores a float as two integers, value is num * (2**exp)"
     #integers
@@ -15,7 +16,7 @@ class APFloat:
         float
         int
         """
-        if(type(num) == APFloat):
+        if(isinstance(num,APFloat)):
             self.num = num.num
             self.maxprecision = num.maxprecision
             self.exponent = num.exponent
@@ -23,7 +24,7 @@ class APFloat:
             return
         self.maxprecision = precision
         #one parameter is given
-        if(not exp):
+        if(exp==None):
             #case float
             #floats have 53 bits of precision
             if(isinstance(num,float)):
@@ -34,21 +35,31 @@ class APFloat:
             #case int is given
             if(isinstance(num,int)):
                 self.num = num
+                if(num == 0):
+                    self.exponent = -float("inf")
+                    self.maxprecision = float("inf")
+                    return
                 self.exponent = 0
-                self.__ensurePrecision()
+
+                self.maxprecision = float("inf")
                 return
         else:
             self.exponent = exp
-            self.num = num
+            self.num = int(num)
             self.__ensurePrecision()
-
+    def __rmul__(self, other):
+        return self*other
     def __mul__(self, other):
-        if(type(other)!=APFloat):
-            other = APFloat(other)
-        maxprecision = min(self.maxprecision,other.maxprecision)
-        out = APFloat(self.num*other.num,self.exponent+other.exponent,maxprecision)
-        out.__ensurePrecision()
+        if(type(other) == int):
+            out = APFloat(self.num*other,self.exponent,self.maxprecision)
+        else:
+            if(type(other)!=APFloat):
+                other = APFloat(other)
+            maxprecision = min(self.maxprecision,other.maxprecision)
+            out = APFloat(self.num*other.num,self.exponent+other.exponent,maxprecision)
         return out
+    def __rtruediv__(self, other):
+        return APFloat(other)/self
     def __truediv__(self, other):
         if(type(other)!=APFloat):
             other = APFloat(other)
@@ -56,20 +67,23 @@ class APFloat:
         maxval = min(self.maxval,other.maxval)
         out = APFloat(self.num*maxval
                       //(other.num), self.exponent-other.exponent-maxprecision,maxprecision)
-        out.__ensurePrecision()
 
         return out
 
     #The lower exponent wins, lower precision wins
-    def __add__(self, other):
+    def __radd__(self, other):
+        return self+other
+    def __add__(self, other:(int,float)):
         if(type(other)!=APFloat):
             other = APFloat(other)
+
         precision = min(self.maxprecision,other.maxprecision)
         exp = max(self.exponent,other.exponent)
         num = self.num // 2**(exp-self.exponent)+other.num // 2**(exp-other.exponent)
         out = APFloat(num,exp,precision)
-        out.__ensurePrecision()
         return out
+    def __rsub__(self,other):
+        return APFloat(other)-self
     def __sub__(self, other):
         if(type(other)!=APFloat):
             other = APFloat(other)
@@ -77,14 +91,16 @@ class APFloat:
         exp = max(self.exponent,other.exponent)
         num = self.num // 2**(exp-self.exponent)-other.num // 2**(exp-other.exponent)
         out = APFloat(num,exp,precision)
-        out.__ensurePrecision()
         return out
     def __neg__(self):
         out = APFloat(self)
         out.num *=-1
         return out
     def __str__(self):
-        return "{}".format(float(self))
+        try:
+            return "{}".format(float(self))
+        except(OverflowError):
+            return "{}*2**{}".format(self.num,self.exponent)
     #gt -> self > other
     #self < other is by default implemented by calling other > self
     def __gt__(self, other):
@@ -142,21 +158,46 @@ class APFloat:
                 elif(selfexp == otherexp):
                     return self.num>other.num
             return False
+
     def __eq__(self, other):
-        print((self-other).num)
-        if(self - other):
-            return False
-        return True
+        if(type(other)!=APFloat):
+            if(type(other) in (int,float,complex)):
+                other = APFloat(other)
+            else:
+                return False
+        if(self.num == other.num and self.exponent == other.exponent and self.__maxprecision == other.__maxprecision):
+            return True
+        return False
     def __ensurePrecision(self):
-        if(self.num == 0): return
-        while(abs(self.num)>self.maxval):
-            self.num//=2
-            self.exponent+=1
-            #print("high",self.num)
-        while(abs(self.num)<self.maxval):
-            self.num*=2
-            self.exponent-=1
-            #print("low")
+
+        oldexp = self.exponent
+        if(self.num == 0):
+            self.exponent = -float("inf")
+            return
+        if(self.maxprecision == float("inf")):
+            return
+        t = time.time()
+        p = self.__maxprecision
+        siz = sys.getsizeof(self.num)
+
+        offset = ((siz-28)//4)*30 - p
+        t = time.time()-t
+        #if(t>0):
+            #print("ensure took: ",t,"seconds with",oldexp,"->",self.exponent)
+            #print("siz:",siz,"offset:",offset)
+        if(offset>0):
+            self.num>>=offset #divide by 2**offset
+            self.exponent+=offset
+        if(offset<0):
+            self.num<<=-offset #multiply by 2**offset
+            self.exponent+=offset
+        offset2 = int(math.ceil(math.log2(abs(self.num)) - self.maxprecision))
+        self.num>>=offset2
+        self.exponent += offset2
+        pass
+
+
+
     def __float__(self):
         return float(self.num*2**(self.exponent))
     def __int__(self):
@@ -172,7 +213,15 @@ class APFloat:
         print("problem too hard")
         return 10
     def sqrt(self):
-        result = APFloat(self)
+        result = APFloat(self/2)
+
+        while (True):
+            oldresult = result
+            result = result/2 + self/(2*result)
+            if (result == oldresult):
+                return result
+
+
     def reciprocal(self):
         return APFloat(1,precision=self.maxprecision)/self
 
@@ -181,30 +230,42 @@ class APFloat:
         out = APFloat(0,precision = self.maxprecision)
         term = APFloat(1,precision = self.maxprecision)
         while(True):
-            old = APFloat(out)
-            print(term)
+            old = out
             out += term
-            print(out-old)
-
             if(out == old): return out
-
             term *= self/i
             i+=1
 
     def ln(self):
         #newton's method
+        '''
         x0 = APFloat(self)
         for i in range(100):
             x0 += x0.exp().reciprocal()*self -1
         return x0
+        '''
+        #print("self",self)
+        x = (self-1)/(self+1)
+        #calculates ln(1+x/1-x) = 2*(x + x^3/3 + x^5/5 + x^7/7 + ...)
+        term = APFloat(2*x)
+        x2 = x*x #x^2
+        out = APFloat(0)
+        i = 1
+        while (True):
+            old = out
+            out += term/i
+            if(old == out): return out
+            term *= x2 # x, x^3, x^5, x^7....
+            i += 2     # 1, 3  , 5  , 7  ....
 
     @property
     def maxprecision(self):
+
         return self.__maxprecision
     @maxprecision.setter
     def maxprecision(self,val:int):
-        print("setPrecision",val)
-        if(type(val)!=int):
+        #print("setPrecision",val)
+        if(type(val)!=int and val != float("inf")):
             print("val expected int, got " + str(type(val)))
         self.__maxprecision = val
         self.maxval = 2**val
@@ -215,16 +276,27 @@ class APFloat:
 
 
 if __name__ == "__main__":
-    a,b,c,d = 100,3,20,3
+    import time
 
-    f = APFloat(a,c,1000)
-    f1 = APFloat(b,d,100)
-    print("f1",f1)
-    print("f",f)
+    a,b,c,d = 100,34,-20,-3
+
+    f = APFloat(a,c,500)
+    f1 = APFloat(b,d,500)
+    print("f1",f1,b*2**d)
+    print("f",f,a*2**c)
     print("+",f+f1)
     print(float((a*2**c+b*2**d)))
-    print("*",f*f1)
-    print(float((a*2**c*b*2**d)))
+    t = time.time()
+    x = f1*f
+    t = time.time()-t
+
+    print(x,"*",t)
+    t = time.time()
+    x = (a*2**c) * (b*2**d)
+    t = time.time()-t
+
+    print(x,t)
+
     print("/",f/f1)
     print(float(((a*2**c)/(b*2**d))))
     print(">",f>f1)
@@ -233,10 +305,41 @@ if __name__ == "__main__":
     print((a*2**c)<(b*2**d))
     print("==",f==f1)
     print((a*2**c)==(b*2**d))
-    print(f1.exp(),"exp")
-    print(math.exp(b*2**d))
-    print(f1.ln(),"ln")
-    print(math.log(b*2**d))
+    print("+",1+f1)
+    print(1+(b*2**d))
+    print("==",1/f1)
+    print(1/(b*2**d))
+    print("==",f==f1)
+    print((a*2**c)==(b*2**d))
+
+
+    t = time.time()
+    x = f1.exp()
+    t = time.time()-t
+
+    print(x,"exp",t)
+    t = time.time()
+    x = math.exp(b*2**d)
+    t = time.time()-t
+
+
+    print(x,"exp",t)
+
+    t = time.time()
+    x = f1.ln()
+    t = time.time()-t
+
+    print(x,"ln",t)
+
+    t = time.time()
+    x = math.log(b*2**d)
+    t = time.time()-t
+    print(x,"ln",t)
+
+    print("sqrt",f.sqrt())
+    print(math.sqrt(a*2**c))
+
+
 
 #probably better and faster than int based APFoat
 '''
